ce8ca8134ae74d1a4dc50a9b4b45517e26f7d89f
Tom Gao <tom@tomgao.cc>
2024-05-17 18:25:10 +0800

    chore: remove types

--- packages/eslint/vite.config.ts
+++ packages/eslint/vite.config.ts
@@ -4,9 +4,7 @@ import { buildPlugin } from 'vite-plugin-build';
 export default defineConfig({
   plugins: [
     buildPlugin({
-      fileBuild: {
-        emitDeclaration: true,
-      },
+      fileBuild: {},
     }),
   ],
 });
