b7d2e1c50b4b5cb0b9e16383a008369db766b294
Tom Gao <tom@tomgao.cc>
2024-05-30 12:10:42 +0800

    feat(commitlint): add header-max-length-plus rule

--- packages/commitlint/src/index.ts
+++ packages/commitlint/src/index.ts
@@ -1,10 +1,11 @@
+import type { UserConfig } from '@commitlint/types';
+
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-trim': [2, 'always'],
-    'header-max-length': [2, 'always', 120],
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
     'subject-case': [0],
@@ -25,5 +26,22 @@ export default {
         'revert',
       ],
     ],
+    'header-max-length': [0],
+    'header-max-length-plus': [2, 'always', 120],
   },
-};
+  plugins: [
+    {
+      rules: {
+        'header-max-length-plus': (parsed, when, value) => {
+          const maxLength = value || 200;
+          const header = parsed.header || '';
+          const length = header.length;
+          return [
+            header.trim().startsWith('chore: release') ? true : length <= maxLength,
+            `header must not be longer than ${maxLength} characters, current length is ${length}`,
+          ];
+        },
+      },
+    },
+  ],
+} as UserConfig;
